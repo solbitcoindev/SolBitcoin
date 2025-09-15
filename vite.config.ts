6aa6759972c7b94c4bef960fdc19441932862f78
solbitcoindev <solbitcoindev@gmail.com>
2025-09-15 16:31:26 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-     allowedHosts: ["c17e126e5e5b.ngrok-free.app"],
+    allowedHosts: ["c17e126e5e5b.ngrok-free.app"],
   },
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
