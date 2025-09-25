da6b9d29b81ccea4986ae4412f75170907f10f64
solbitcoindev <solbitcoindev@gmail.com>
2025-09-25 16:58:24 +0300

    mobile adapt v3

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,7 +3,10 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const getIsMobile = () =>
+    typeof window !== "undefined" ? window.innerWidth < MOBILE_BREAKPOINT : false
+
+  const [isMobile, setIsMobile] = React.useState<boolean>(getIsMobile)
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
@@ -11,9 +14,10 @@ export function useIsMobile() {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
     mql.addEventListener("change", onChange)
+    // Sync in case something changed between render and effect
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
